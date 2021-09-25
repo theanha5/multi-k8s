b30df73fd38eb2b36e10bfdba35cd1ac2ee5b567
anhnt2310 <anhnt@toprate.io>
2021-09-25 22:32:16 +0700

    update docker id

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,17 @@
-docker build -t theanha5/multi-client:latest -t theanha5/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t theanha5/multi-server:latest -t theanha5/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t theanha5/multi-worker:latest -t theanha5/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t anhnt2310/multi-client:latest -t anhnt2310/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t anhnt2310/multi-server:latest -t anhnt2310/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t anhnt2310/multi-worker:latest -t anhnt2310/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
-docker push theanha5/multi-client:latest
-docker push theanha5/multi-server:latest
-docker push theanha5/multi-worker:latest
+docker push anhnt2310/multi-client:latest
+docker push anhnt2310/multi-server:latest
+docker push anhnt2310/multi-worker:latest
 
-docker push theanha5/multi-client:$SHA
-docker push theanha5/multi-server:$SHA
-docker push theanha5/multi-worker:$SHA
+docker push anhnt2310/multi-client:$SHA
+docker push anhnt2310/multi-server:$SHA
+docker push anhnt2310/multi-worker:$SHA
 
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=theanha5/multi-server:$SHA
-kubectl set image deployments/client-deployment client=theanha5/multi-client:$SHA
-kubectl set image deployments/worker-deployment worker=theanha5/multi-worker:$SHA
+kubectl set image deployments/server-deployment server=anhnt2310/multi-server:$SHA
+kubectl set image deployments/client-deployment client=anhnt2310/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=anhnt2310/multi-worker:$SHA
